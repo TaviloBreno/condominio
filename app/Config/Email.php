08ca431b102ba6f78b90173ca387e5588d14daf2
Tavilo Breno <breno_wk2@hotmail.com>
2026-03-05 18:54:49 -0300

    Instalando e configurando a lib shield

--- app/Config/Email.php
+++ app/Config/Email.php
@@ -6,8 +6,8 @@ use CodeIgniter\Config\BaseConfig;
 
 class Email extends BaseConfig
 {
-    public string $fromEmail  = '';
-    public string $fromName   = '';
+    public string $fromEmail  = 'adm@adm.com';
+    public string $fromName   = 'adm';
     public string $recipients = '';
 
     /**
