a7d553e38e71c19d5ad60aff2b79a6550042906a
Tavilo Breno <breno_wk2@hotmail.com>
2026-09-23 02:21:51 -0300

    feat: add resident management module with model, controller, views, and layout

--- app/Config/Routes.php
+++ app/Config/Routes.php
@@ -10,4 +10,18 @@ use CodeIgniter\Router\RouteCollection;
  */
 $routes->get('/', [HomeController::class, 'index'], ['as' => 'home']);
 
+// Módulo: Residentes
+$routes->group('residentes', static function ($routes) {
+    $routes->get('', 'ResidentesController::index', ['as' => 'residentes.index']);
+    $routes->get('novo', 'ResidentesController::novo', ['as' => 'residentes.novo']);
+    $routes->post('criar', 'ResidentesController::criar', ['as' => 'residentes.criar']);
+    $routes->get('detalhes/(:num)', 'ResidentesController::detalhes/$1', ['as' => 'residentes.detalhes']);
+    $routes->get('editar/(:num)', 'ResidentesController::editar/$1', ['as' => 'residentes.editar']);
+    $routes->post('atualizar/(:num)', 'ResidentesController::atualizar/$1', ['as' => 'residentes.atualizar']);
+    $routes->post('excluir/(:num)', 'ResidentesController::excluir/$1', ['as' => 'residentes.excluir']);
+    $routes->post('toggle-status/(:num)', 'ResidentesController::toggleStatus/$1', ['as' => 'residentes.toggleStatus']);
+    $routes->post('usuario/(:num)/criar', 'ResidentesController::criarUsuario/$1', ['as' => 'residentes.criarUsuario']);
+    $routes->post('usuario/(:num)/toggle-acesso', 'ResidentesController::toggleAcessoUsuario/$1', ['as' => 'residentes.toggleAcessoUsuario']);
+});
+
 service('auth')->routes($routes);
